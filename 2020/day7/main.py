1779fbf345835c109ac6d6ae30b5f6db6d39bfc9
Xiang Chen <xiangchenchen96@gmail.com>
2020-12-08 02:27:41 +0100

    Day7

--- 2020/day7/main.py
+++ 2020/day7/main.py
@@ -1,3 +1,4 @@
+import re
 from typing import List
 
 
@@ -20,7 +21,23 @@ def part_1(rules: List[str]) -> int:
 
 
 def part_2(rules: List[str]) -> int:
-    pass
+    relations = {}
+    parent_pattern = re.compile(r"^(\w+ \w+)")
+    child_pattern = re.compile(r"(\d) (\w+ \w+) bag")
+    for rule in rules:
+        parent = parent_pattern.match(rule).group()
+        children = child_pattern.findall(rule)
+        relations[parent] = {bag: int(n) for n, bag in children}
+    return count_bags(relations, "shiny gold")
+
+
+def count_bags(relations, color):
+    if not relations[color]:
+        return 1
+    result = 1 if color != "shiny gold" else 0
+    for c, n in relations[color].items():
+        result += n * count_bags(relations, c)
+    return result
 
 
 if __name__ == "__main__":
