7af1b03f226d1b15d7bf147b342054014827d032
Xiang Chen <xiangchenchen96@gmail.com>
2020-12-11 18:37:37 +0100

    Day11 part2

--- 2020/day11/main.py
+++ 2020/day11/main.py
@@ -20,17 +20,47 @@ def iterate(seats: dict):
     return new_seats
 
 
+def iterate2(seats: dict):
+    new_seats = copy.deepcopy(seats)
+    for (x, y), v in seats.items():
+        if v == ".":
+            continue
+        occupied_visible = 0
+        for dx, dy in adjacent:
+            nx, ny = x + dx, y + dy
+            while (nx, ny) in seats:
+                if seats[nx, ny] == "#":
+                    occupied_visible += 1
+                    break
+                elif seats[nx, ny] == "L":
+                    break
+                nx, ny = nx + dx, ny + dy
+        if v == "L" and not occupied_visible:
+            new_seats[x, y] = "#"
+        elif v == "#" and occupied_visible >= 5:
+            new_seats[x, y] = "L"
+    # x, y = max(seats)
+    # for i in range(x + 1):
+    #     print([seats[i, j] for j in range(y + 1)])
+    # print()
+    return new_seats
+
+
 def part_1(seats: dict):
     new_seats = iterate(seats)
     while seats != new_seats:
         seats = new_seats
         new_seats = iterate(seats)
-
     return len([v for v in seats.values() if v == "#"])
 
 
 def part_2(seats: dict):
-    pass
+    new_seats = iterate(seats)
+    while seats != new_seats:
+        seats = new_seats
+        new_seats = iterate2(seats)
+
+    return len([v for v in seats.values() if v == "#"])
 
 
 if __name__ == "__main__":
