b01d27b866a212d128bc636e62766d858ead4a0e
Xiang Chen <xiangchenchen96@gmail.com>
2020-12-13 13:31:58 +0100

    Add day13 part 1

---
+++ 2020/day13/main.py
@@ -0,0 +1,33 @@
+from typing import List
+
+
+def get_first_bus(min_time, bus):
+    result = bus
+    while result < min_time:
+        result += bus
+    return result
+
+
+def part_1(min_time: int, busses: List[str]):
+    busses = [int(b) for b in busses if b != "x"]
+    result = busses[0]
+    for b in busses:
+        time = get_first_bus(min_time, b)
+        if time < get_first_bus(min_time, result):
+            result = b
+    return result * (get_first_bus(min_time, result) - min_time)
+
+
+def part_2(min_time: int, busses: List[str]):
+    pass
+
+
+if __name__ == "__main__":
+    with open("data.txt") as f:
+        data = f.read()
+    data = data.split("\n")
+    earliest = int(data[0])
+    all_busses = data[1].split(",")
+
+    print(part_1(earliest, all_busses))
+    print(part_2(earliest, all_busses))
