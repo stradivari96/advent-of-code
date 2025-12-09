0171d705a929bbada2417a804ea896f6868d74cf
Xiang Chen <xiang1016127637@gmail.com>
2025-12-09 23:02:17 +0100

    Clean

--- 2025/day09/main.py
+++ 2025/day09/main.py
@@ -40,30 +40,12 @@ def part_2(problem: Problem, heap):
             x_start, x_end = sorted((u_x, v_x))
             y_start, y_end = sorted((u_y, v_y))
             # all edges are either vertical or horizontal!!
-            if x_start == x_end:
-                # this is | vertical edge, x is constant
-                if x_start <= x1:
-                    continue  # to the left of rectangle
-                if x_start >= x2:
-                    continue  # to the right of rectangle
-                if y_end <= y1:
-                    continue  # below rectangle
-                if y_start >= y2:
-                    continue  # above rectangle
-                # intersects
-                return False
-            else:
-                # this is - horizontal edge, y is constant
-                if y_start >= y2:
-                    continue  # above rectangle
-                if y_start <= y1:
-                    continue  # below rectangle
-                if x_end <= x1:
-                    continue  # left of rectangle
-                if x_start >= x2:
-                    continue  # right of rectangle
-                # intersects
-                return False
+            if x_end <= x1 or x_start >= x2:
+                continue  # to the left or to the right
+            if y_end <= y1 or y_start >= y2:
+                continue  # above or below
+            # intersects
+            return False
         return True
 
     # Iterate through rectangles from largest to smallest
